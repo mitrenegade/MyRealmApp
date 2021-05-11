c3c67392d4f71ad3d61799eedf78b0c5e4077ff7
Bobby Ren <bobbyren@gmail.com>
2021-05-10 21:08:43 -0700

    Added Realm Pod

---
+++ RealmTestApp/RealmManager.swift
@@ -0,0 +1,19 @@
+//
+//  RealmManager.swift
+//  RealmTestApp
+//
+//  Created by Bobby Ren on 5/10/21.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    private let realm: Realm
+    init() {
+        guard let realm = try? Realm() else {
+            fatalError("Realm could not be instantiated")
+        }
+        self.realm = realm
+    }
+}
